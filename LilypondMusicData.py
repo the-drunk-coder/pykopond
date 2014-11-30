1b14d8e6de98f88892f93c5340cf403eff95ee03
nik <nik@parkellipsen.de>
2014-11-30 13:44:46 +0100

    fixed defaults and end matching

--- LilypondMusicData.py
+++ LilypondMusicData.py
@@ -157,13 +157,13 @@ class Rest(Note):
         
 class LilypondScore():
     def __init__(self, *args, **kwargs):
-        self.title = kwargs.get('title', "some title")
-        self.dedication = kwargs.get('dedication', "some dedication")
-        self.subtitle = kwargs.get('subtitle', "some subtitle")
-        self.subsubtitle = kwargs.get('subsubtitle', "some subsubtitle")
-        self.meter = kwargs.get('meter', "some meter")
-        self.composer = kwargs.get('composer', "some composer")
-        self.copyright = kwargs.get('copyright', "some copyright")
+        self.title = kwargs.get('title', "Title")
+        self.dedication = kwargs.get('dedication', "")
+        self.subtitle = kwargs.get('subtitle', "")
+        self.subsubtitle = kwargs.get('subsubtitle', "")
+        self.meter = kwargs.get('meter', "")
+        self.composer = kwargs.get('composer', "")
+        self.copyright = kwargs.get('copyright', "")
         self.version = kwargs.get('version', "2.18.2")
         self.voices = []
     def __str__(self):
@@ -186,14 +186,14 @@ class LilypondScore():
     def add_voices(self, voices):
         self.voices.extend(voices)
     def output_ly(self):
-        filename = (self.composer + "_-_" + self.title).replace(" ", "_") + ".ly"
+        filename = (self.composer + "_-_" + self.title + "_-_" + self.subtitle).replace(" ", "_") + ".ly"
         if not os.path.exists("ly"):
             os.makedirs("ly")
         score_file = open("ly/" + filename, 'w')
         score_file.write(str(self))
         score_file.close()
     def output_pdf(self):
-        filename = (self.composer + "_-_" + self.title).replace(" ", "_") + ".ly"
+        filename = (self.composer + "_-_" + self.title + "_-_" + self.subtitle).replace(" ", "_") + ".ly"
         self.output_ly()
         #actually output to file
         if not os.path.exists("pdf"):
@@ -241,15 +241,21 @@ class LilypondVoice():
                 original_note = copy.deepcopy(note)
                 split_note.duration = split_note.duration - current_bar_remainder
                 original_note.duration = current_bar_remainder
-                print("SPLT NOTE: {0} : {1}".format(note.duration, split_note.duration))
-                bars += str(original_note) + " ~ | % " + " " + str(bar_count) + "\n"
+                #print("SPLT NOTE: {0} : {1}".format(note.duration, split_note.duration))
+
+                if(original_note.pitch_class == REST):
+                    bars += str(original_note) + " | % " + " " + str(bar_count) + "\n"
+                else:
+                    bars += str(original_note) + " ~ | % " + " " + str(bar_count) + "\n"
+
                 bars += "% SPLIT POINT\n"
                 bars += str(split_note) + " "
                 lyrics_bars += note.syllable + " | %" + " " + str(bar_count) + "\n"
-                lyrics_bars += "% SPLIT POINT\n"
+                #lyrics_bars += "% SPLIT POINT\n"
                 current_bar_remainder = self.bar_size - split_note.duration
                 bar_count += 1
         inner_voice_string = lilypond_inner_voice_template.format(self.short_name, self.clef,int(self.time_signature[0]), int(Decimal("1.0") / self.time_signature[1]), bars)
+        # assemble lyrics templates
         inner_lyrics_string = " "
         if self.contains_lyrics:
             inner_lyrics_string = lilypond_inner_lyrics_template.format(self.short_name, self.clef, int(self.time_signature[0]), int(Decimal("1.0") / self.time_signature[1]), lyrics_bars)
@@ -262,9 +268,11 @@ class LilypondTools():
     def match_end(self, voices):
         # find longest voice
         longest_voice = voices[0]
+        longest_voice_index = 0
         for voice_ptr in range(0,len(voices)):
             if (voices[voice_ptr]).total_duration > longest_voice.total_duration:
                 longest_voice = voices[voice_ptr]
+                longest_voice_index = voice_ptr
         # remove longest voice from list (temporariliy)
         voices.remove(longest_voice)
         # match shorter voices to longer voice       
@@ -284,17 +292,17 @@ class LilypondTools():
             if total_remainder != Decimal('0.0'):
                voice.add_note(Rest(total_remainder))
             
-        # finally, add voice to list of voices again
-        voices.append(longest_voice)
-        
+        # finally, add voice to list of voices again, keeping the original order
+        voices.insert(longest_voice_index, longest_voice)
+    
         
     # if the voice end on some crude measure, pad it to the next full bar
     def flush_end_to_bar(self, voice):
         bar_rest = voice.total_duration % voice.bar_size
-        print("TOTAL DUR: " + str(voice.total_duration))
-        print("BAR SIZE: " + str(voice.bar_size))
-        print("BAR REST:" + str(bar_rest))
-        print("DIFFERENCE: " + str(voice.bar_size - bar_rest))
+        #print("TOTAL DUR: " + str(voice.total_duration))
+        #print("BAR SIZE: " + str(voice.bar_size))
+        #print("BAR REST:" + str(bar_rest))
+        #print("DIFFERENCE: " + str(voice.bar_size - bar_rest))
         if bar_rest != Decimal("0.0"):
            voice.add_note(Rest(voice.bar_size - bar_rest))
      
